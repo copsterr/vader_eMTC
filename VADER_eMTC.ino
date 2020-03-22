76b6d0e21e4c4b38f26192a57980309b5faa6921
Siraphob Kanjanasaensong <copsterr@gmail.com>
2020-03-22 21:35:16 +0700

    add RTC and PMS code

--- VADER_eMTC.ino
+++ VADER_eMTC.ino
@@ -1,5 +1,8 @@
 #include "ct_AIS_BG96.h"
 #include "device_config.h"
+#include "PMS.h"
+#include <ThreeWire.h>  
+#include <RtcDS1302.h>
 
 /* Private Defines -------------- */
 #define APN               "aistest.emtc"
@@ -12,8 +15,20 @@ void (* resetFunc)(void) = 0;
 void resetArduino(void);
 
 /* PV --------------------------- */
+// PMS variables
+PMS pms(Serial1);
+PMS::DATA pmsData;
+
+// RTC variables
+ThreeWire myWire(51,52,53); // IO, SCLK, CE
+RtcDS1302<ThreeWire> Rtc(myWire);
+
+// eMTC vars
+uint32_t prevMillis = millis();
 connect_status_t connect_status = CONNECT_STATUS_UNKNOWN_ERR;
 uint8_t pingCount = 0;
+#define DUTY_CYCLE 5000
+
 
 void setup() {
   Serial.begin(VC_BAUD);
@@ -36,27 +51,85 @@ void setup() {
   } while (connect_status != CONNECT_STATUS_OK);
 
   // Ping to check connection with the remote server
-  while (pingCount != PING_ATTEMPTS_MAX) {
+  while (pingCount < PING_ATTEMPTS_MAX) {
     if (pingServer("178.128.16.9") != 0) {
       Serial.println("Ping Error! Re-pinging...");
       pingCount++;
       delay(2000);
     }
     else {
+      pingCount = 0;
       Serial.println("Ping Success!");
       break;
     }
   }
+  if (pingCount == 3) {
+    Serial.println("Connection Failed.");
+    resetArduino();
+  }
+
+  // init PMS Serial port
+  Serial1.begin(9600);
   
 
 }
 
 
 void loop() {
-  if (pingServer("178.128.16.9") == 0) {
-    Serial.println("Ping success");
-  };
-  delay(5000);
+  
+  if (pms.read(pmsData))
+  {
+//    Serial.println(pmsData.PM_AE_UG_1_0);
+//    Serial.println(pmsData.PM_AE_UG_2_5);
+//    Serial.println(pmsData.PM_AE_UG_10_0);
+
+    // Ping to check connection with the remote server
+    while (pingCount < PING_ATTEMPTS_MAX) {
+      if (pingServer("178.128.16.9") != 0) {
+        Serial.println("Ping Error! Re-pinging...");
+        pingCount++;
+        delay(2000);
+      }
+      else {
+        pingCount = 0;
+        Serial.println("Connected to eMTC.");
+        break;
+      }
+    }
+    if (pingCount == 3) {
+      Serial.println("Connection Failed.");
+      resetArduino();
+    }
+
+    // get date and time from RTC
+    RtcDateTime now = Rtc.GetDateTime();
+
+    // get time to payload
+    char timePayload[80] = {0};
+    sprintf(timePayload, "%02u/%02u/%04u %02u:%02u:%02u",
+            now.Month(),
+            now.Day(),
+            now.Year(),
+            now.Hour(),
+            now.Minute(),
+            now.Second());
+
+
+    String payload = "{\"dev_id\":\"" + String(DEV_ID) + \
+                     "\",\"type\":\"PM\",\"timestamp\":\"" + String(timePayload) + \
+                     "\",\"PM1_0\":" + String(pmsData.PM_AE_UG_1_0) + \
+                     ",\"PM2_5\":" + String(pmsData.PM_AE_UG_2_5) + \
+                     ",\"PM10_0\":" + String(pmsData.PM_AE_UG_10_0) + \
+                     ",\"Lat\":" + "" + \
+                     ",\"Lng\":" + "" + "}";
+
+    // transmit data
+    if (millis() - prevMillis > DUTY_CYCLE) {
+      sendData(payload);
+      prevMillis = millis();
+    }
+    
+  }
 }
 
 
